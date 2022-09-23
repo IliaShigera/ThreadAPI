4791d24c059cfd64c647ffcea7d9bc477bf7553f
Ilia Shigera <shigerailia@gmail.com>
2022-09-23 21:15:13 +0200

    UserAccess: added UniqueUser implementation

---
+++ src/Modules/UserAccess/Thread.Modules.UserAccess.Infrastructure/Domain/UniqueUser.cs
@@ -0,0 +1,16 @@
+﻿namespace Thread.Modules.UserAccess.Infrastructure.Domain;
+
+public class UniqueUser: IUniqUser
+{
+    private readonly IUserAccessDbContext _context;
+
+    public UniqueUser(IUserAccessDbContext context)
+    {
+        _context = context;
+    }
+
+    public bool IsUniqEmail(string email)
+    {
+        return _context.Registrations.Any(r => r.Email.Equals(email));
+    }
+}
