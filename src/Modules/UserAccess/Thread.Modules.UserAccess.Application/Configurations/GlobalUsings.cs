572c23049bf3c27b0fa9848a48233a0be6a9cb45
Ilia Shigera <shigerailia@gmail.com>
2022-09-23 22:58:42 +0200

    UserAccess: added IDomainEventHandler

--- src/Modules/UserAccess/Thread.Modules.UserAccess.Application/Configurations/GlobalUsings.cs
+++ src/Modules/UserAccess/Thread.Modules.UserAccess.Application/Configurations/GlobalUsings.cs
@@ -5,6 +5,7 @@ global using Thread.Modules.UserAccess.Application.Configurations;
 global using Thread.Modules.UserAccess.Application.Configurations;
 global using Thread.Modules.UserAccess.Application.Contracts;
 global using Thread.Modules.UserAccess.Domain.Models;
+global using Thread.Modules.UserAccess.Domain.Configuration;
 
 // others
 global using Microsoft.EntityFrameworkCore;
---
+++ src/Modules/UserAccess/Thread.Modules.UserAccess.Application/Configurations/IDomainEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Thread.Modules.UserAccess.Application.Configurations;
+
+public interface IDomainEventHandler<in TDomainEvent> : 
+    INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent
+{
+}
