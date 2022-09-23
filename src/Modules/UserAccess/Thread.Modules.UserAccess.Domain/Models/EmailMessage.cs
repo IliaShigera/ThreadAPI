a2fa522e080c1fd26e717cefd07c7cfd554d4926
Ilia Shigera <shigerailia@gmail.com>
2022-09-23 23:05:07 +0200

    UserAccess: added EmailMessage constructor

--- src/Modules/UserAccess/Thread.Modules.UserAccess.Domain/Models/EmailMessage.cs
+++ src/Modules/UserAccess/Thread.Modules.UserAccess.Domain/Models/EmailMessage.cs
@@ -2,6 +2,13 @@
 
 public struct EmailMessage
 {
+    public EmailMessage(string to, string subject, string content)
+    {
+        To = to;
+        Subject = subject;
+        Content = content;
+    }
+
     public string To;
     public string Subject;
     public string Content;
