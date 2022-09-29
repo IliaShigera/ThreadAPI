d497d7c78fc2a71003e231221a68321dc9811e89
Ilia Shigera <shigerailia@gmail.com>
2022-09-29 09:12:56 +0200

    UserAccess: made SmtpConfiguration poprs public

--- src/Modules/UserAccess/Thread.Modules.UserAccess.Infrastructure/Configuration/SmtpConfiguration.cs
+++ src/Modules/UserAccess/Thread.Modules.UserAccess.Infrastructure/Configuration/SmtpConfiguration.cs
@@ -4,9 +4,9 @@ internal sealed class SmtpConfiguration
 {
     internal const string SECTION_NAME = "SmtpConfig";
     
-    internal string Host { get; set; }
-    internal int Port { get; set; }
-    internal string UserName { get; set; }
-    internal string Email { get; set; }
-    internal string Password { get; set; }
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
