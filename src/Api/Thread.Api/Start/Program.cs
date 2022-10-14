ae2d579ddd80fe0d0431bb8a46904c9aefc3569b
Ilia Shigera <shigerailia@gmail.com>
2022-10-14 23:37:52 +0200

    Api: added empty appSetting files

--- src/Api/Thread.Api/Start/Program.cs
+++ src/Api/Thread.Api/Start/Program.cs
@@ -3,11 +3,12 @@ Log.Logger = Startup.ConfigureLogger();
 try
 {
     Log.Information("Staring web host");
-    
+
     var builder = WebApplication.CreateBuilder(args);
+    builder.Configuration.AddJsonFile($"Configuration/AppSettings/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json");
     Startup.ConfigureHost(builder.Host);
-    Startup.ConfigureWebHost(builder.WebHost, builder.Configuration); 
-    Startup.ConfigureServices(builder.Services, builder.Configuration); 
+    Startup.ConfigureWebHost(builder.WebHost, builder.Configuration);
+    Startup.ConfigureServices(builder.Services, builder.Configuration);
     var app = builder.Build();
     Startup.ConfigureWebApp(app);
     app.Run();
