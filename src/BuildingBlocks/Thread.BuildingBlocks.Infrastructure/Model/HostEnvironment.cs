31b6c7f53d7bb579f608b91f4726648042837af4
Ilia Shigera <shigerailia@gmail.com>
2022-10-21 09:19:39 +0200

    BuildingBlocks: added common HostEnvironment

---
+++ src/BuildingBlocks/Thread.BuildingBlocks.Infrastructure/Model/HostEnvironment.cs
@@ -0,0 +1,18 @@
+﻿namespace Thread.BuildingBlocks.Infrastructure.Model;
+
+public sealed class HostEnvironment
+{
+    private static readonly string HostUriGlobalVariableName = "API_URI";
+    private static readonly string EnvTypeGlobalVariableName = "ASPNETCORE_ENVIRONMENT";
+
+    public static readonly string Uri = System.Environment.GetEnvironmentVariable(HostUriGlobalVariableName)!;
+    public static readonly string EnvironmentType = System.Environment.GetEnvironmentVariable(EnvTypeGlobalVariableName)!;
+
+    public static readonly string Development = nameof(Development);
+    public static readonly string Production = nameof(Production);
+    public static readonly string Test = nameof(Test);
+
+    public static bool IsDevelopment() => String.Equals(EnvironmentType, Development);
+    public static bool IsProduction() => String.Equals(EnvironmentType, Production);
+    public static bool IsTest() => String.Equals(EnvironmentType, Test);
+}
